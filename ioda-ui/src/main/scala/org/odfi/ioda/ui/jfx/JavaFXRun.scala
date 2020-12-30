3f4a6b2b513bf99c55941ad2dab861dd4ae20ae6
richnou <leys.richard@gmail.com>
2020-12-30 15:11:54 +0100

    IODA triggering build

--- ioda-ui/src/main/scala/org/odfi/ioda/ui/jfx/JavaFXRun.scala
+++ ioda-ui/src/main/scala/org/odfi/ioda/ui/jfx/JavaFXRun.scala
@@ -3,7 +3,11 @@ package org.odfi.ioda.ui.jfx
 import javafx.application.Platform
 import com.sun.javafx.tk.Toolkit
 import javafx.application.Application
+import javafx.scene.control.ScrollPane
+import javafx.scene.layout.Region
+import javafx.scene.{Group, Node, Scene}
 import javafx.stage.Stage
+
 import java.util.concurrent.Semaphore
 
 class JFXRun extends Application {
@@ -34,13 +38,15 @@ object JFXRun {
   var semaphore = new Semaphore(0)
   var started = false
   var applicationThread: Option[Thread] = None
-  
+
 
   def stopAll = {
     Platform.setImplicitExit(true)
     Platform.exit()
   }
+
   def noImplicitExit = Platform.setImplicitExit(false)
+
   def setImplicitExit = Platform.setImplicitExit(true)
 
   def waitStarted = started match {
@@ -51,7 +57,9 @@ object JFXRun {
       var fxThread = new Thread(new Runnable() {
         def run = {
 
-          try { Application.launch(classOf[JFXRun]) } finally {}
+          try {
+            Application.launch(classOf[JFXRun])
+          } finally {}
 
         }
       })
@@ -66,57 +74,107 @@ object JFXRun {
   def onJavaFXBlock[T](cl: => T): Option[T] = {
     onJavaFX[T](cl, true)
   }
+
   def onJavaFX[T](cl: => T, block: Boolean = false): Option[T] = {
 
-    //println(s"Running JFX: " + started)
-    started match {
+    println(s"Running JFX: " + Platform.isFxApplicationThread)
+    Platform.isFxApplicationThread match {
       case true =>
 
-        //println(s"Started, trying on JFX Thread")
+
+        var currentSem = new Semaphore(0)
 
         var r: Option[T] = None
-        var currentSem = new Semaphore(0)
-        Platform.runLater(new Runnable() {
-          def run = {
-            // println(s"Running on JFX Platform")
-            try { r = Some(cl) } finally { currentSem.release }
+        try {
+          r= Some(cl)
+        } finally {
+          currentSem.release
+        }
 
-          }
-        })
+
         if (block) {
           currentSem.acquire()
         }
-        //println(s"finished on javafx with block: " + block)
-        //semaphore.acquire()
+
         r
 
-      // No grants in semaphore, start application
       case false =>
+        started match {
+          case true =>
 
-        // Our Main app does release a credit in the semaphore
-        waitStarted
+            //println(s"Started, trying on JFX Thread")
 
-        var r: Option[T] = None
-        var currentSem = new Semaphore(0)
-        Platform.runLater(new Runnable {
+            var r: Option[T] = None
+            var currentSem = new Semaphore(0)
+            Platform.runLater(new Runnable() {
+              def run = {
+                // println(s"Running on JFX Platform")
+                try {
+                  r = Some(cl)
+                } finally {
+                  currentSem.release
+                }
 
-          def run = {
-            try { r = Some(cl) } finally { currentSem.release }
-          }
-        })
+              }
+            })
+            if (block) {
+              currentSem.acquire()
+            }
+            //println(s"finished on javafx with block: " + block)
+            //semaphore.acquire()
+            r
 
-        if (block) {
-          currentSem.acquire()
+          // No grants in semaphore, start application
+          case false =>
+
+            // Our Main app does release a credit in the semaphore
+            waitStarted
+
+            var r: Option[T] = None
+            var currentSem = new Semaphore(0)
+            Platform.runLater(new Runnable {
+
+              def run = {
+                try {
+                  r = Some(cl)
+                } finally {
+                  currentSem.release
+                }
+              }
+            })
+
+            if (block) {
+              currentSem.acquire()
+            }
+            //println(s"finished on javafx with block: " + block)
+
+            // Acquire a semaphore to wait for the end of execution
+            //semaphore.acquire()
+            //semaphore.drainPermits()
+            r
+
         }
-        //println(s"finished on javafx with block: " + block)
-
-        // Acquire a semaphore to wait for the end of execution
-        //semaphore.acquire()
-        //semaphore.drainPermits()
-        r
-
     }
 
+
+  }
+
+  def onStageUI[T <: Region](ui: T)(cl: (Stage, T) => Any) = {
+    JFXRun.onJavaFX {
+
+
+      val root = new ScrollPane()
+      root.setContent(ui)
+      root.fitToHeightProperty().setValue(true)
+      root.fitToWidthProperty().setValue(true)
+      val scene = new Scene(root)
+      val stage = new Stage()
+      stage.setScene(scene)
+      cl(stage,ui)
+
+
+
+    }
   }
 
 }
