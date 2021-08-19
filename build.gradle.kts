33321c412c1c743e6666a3aae376307ff33d3261
richnou <leys.richard@gmail.com>
2021-08-19 19:30:16 +0200

    Next snapshot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@
 
 var scala_major by extra("2.13")
 var scala_minor by extra("6")
-var lib_version by extra("1.1.2-SNAPSHOT")
+var lib_version by extra("1.1.3-SNAPSHOT")
 var branch by extra {System.getenv("BRANCH_NAME")}
 
 if (branch!=null && branch=="release") {
