fd8f875687e379901eaa8494ac534c029072d730
richnou <leys.richard@gmail.com>
2021-08-19 19:45:20 +0200

    Fixed build script not rempving snapshot from version on release branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@
 
 var scala_major by extra("2.13")
 var scala_minor by extra("6")
-var lib_version by extra("1.1.3-SNAPSHOT")
+var lib_version by extra("1.1.2-SNAPSHOT")
 var branch by extra {System.getenv("BRANCH_NAME")}
 
-if (branch!=null && branch=="release") {
+if (System.getenv().getOrDefault("BRANCH_NAME","dev")=="release") {
     lib_version = lib_version.replace("-SNAPSHOT","")
 }
