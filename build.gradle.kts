0391833881547004db4a7bb03c95548440b5fb74
richnou <leys.richard@gmail.com>
2021-08-22 22:11:37 +0200

    Fixing release branch detection for new KTS build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ var scala_minor by extra("6")
 var lib_version by extra("1.1.2-SNAPSHOT")
 var branch by extra {System.getenv("BRANCH_NAME")}
 
-if (System.getenv().getOrDefault("BRANCH_NAME","dev")=="release") {
+if (System.getenv().getOrDefault("BRANCH_NAME","dev").contains("release")) {
     lib_version = lib_version.replace("-SNAPSHOT","")
 }
 
