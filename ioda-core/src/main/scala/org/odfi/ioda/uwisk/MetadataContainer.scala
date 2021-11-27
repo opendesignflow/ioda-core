022fca00bb16b2a76704766926ef0cca72f14939
richnou <leys.richard@gmail.com>
2021-11-27 20:21:25 +0100

    UWisk metadata container: improved methods structuration to add metadata, added method to delete metadata

--- ioda-core/src/main/scala/org/odfi/ioda/uwisk/MetadataContainer.scala
+++ ioda-core/src/main/scala/org/odfi/ioda/uwisk/MetadataContainer.scala
@@ -1,15 +1,37 @@
 package org.odfi.ioda.uwisk
 
+import org.odfi.ioda.json.JsonExtensions.JsonValueHelperMethods
+
 import javax.json.{Json, JsonValue}
 
 trait MetadataContainer extends MetadataContainerTrait {
 
-  def getMetadataOption(name:String) = this.metadatasAsScala.find(_.id==name)
+  def getMetadataOption(name: String) = this.metadatasAsScala.find(_.id == name)
 
-  def addMetadataFromValue(name: String, value: String) : MetadataContainerTraitmetadata = {
+  def getOrAddMetadata(name: String) = {
+    getMetadataOption(name) match {
+      case Some(m) =>
+        m
+      case None =>
+        val m = this.addMetadata
+        m.id = name
+        m
+    }
 
-    val m = this.addMetadata
-    m.id = name
+  }
+
+  def deleteMetadata(name: String) = {
+    this.getMetadataOption(name) match {
+      case Some(m) =>
+        this.metadatas.remove(m)
+      case None =>
+        false
+    }
+  }
+
+  def addMetadataFromValue(name: String, value: String): MetadataContainerTraitmetadata = {
+
+    val m = this.getOrAddMetadata(name)
     m.value = Json.createValue(value)
     m.`type` = "std:string"
 
@@ -19,8 +41,7 @@ trait MetadataContainer extends MetadataContainerTrait {
 
   def addMetadataFromValue(name: String, value: Int): MetadataContainerTraitmetadata = {
 
-    val m = this.addMetadata
-    m.id = name
+    val m = this.getOrAddMetadata(name)
     m.value = Json.createValue(value)
     m.`type` = "std:int"
 
@@ -28,8 +49,7 @@ trait MetadataContainer extends MetadataContainerTrait {
   }
 
   def addMetadataFromValue(name: String, value: Double): MetadataContainerTraitmetadata = {
-    val m = this.addMetadata
-    m.id = name
+    val m = this.getOrAddMetadata(name)
     m.value = Json.createValue(value)
     m.`type` = "std:double"
 
@@ -37,18 +57,27 @@ trait MetadataContainer extends MetadataContainerTrait {
   }
 
   def addMetadataFromValue(name: String, value: Long): MetadataContainerTraitmetadata = {
-    val m = this.addMetadata
-    m.id = name
+    val m = this.getOrAddMetadata(name)
     m.value = Json.createValue(value)
     m.`type` = "std:long"
 
     m
   }
+
   def addMetadataFromValue(name: String, value: Boolean): MetadataContainerTraitmetadata = {
-    val m = this.addMetadata
-    m.id = name
+
+    val m = this.getOrAddMetadata(name)
     m.value = if (value) JsonValue.TRUE else JsonValue.FALSE
     m.`type` = "std:boolean"
+
+    m
+  }
+
+  def addMetadataFromValue(name: String, value: JsonValue): MetadataContainerTraitmetadata = {
+
+    val m = this.getOrAddMetadata(name)
+    m.value = value
+    m.`type` = value.toStdTypeDefinition
 
     m
   }
